1aff7ef03eaeadd776ebb8a44ef2b1444a03fb6a
ldnminh31 <ledoannhatminh31@gmail.com>
2022-05-15 21:26:00 +0700

    confirm logout

--- admin.php
+++ admin.php
@@ -13,11 +13,15 @@ if (!isset($_COOKIE['user']))
     <title>Admin Page</title>
     <link rel="stylesheet" href="./style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
     <?php include_once('./components/sidebar.php') ?>
     <h1 class="animate__animated animate__bounce " style="text-align: center">Welcome admin!</h1>
+
+    
+
 </body>
 
 </html>
