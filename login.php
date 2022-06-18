dc8fafa43636aaa221bb09129542bab305778a5b
Minh Le <ledoannhatminh31@gmail.com>
2022-06-18 14:55:56 +0700

    fix

--- login.php
+++ login.php
@@ -6,9 +6,9 @@ const ADMIN_PSW = "123456";
 $username =  $_POST['username'];
 $password = $_POST['password'];
 if ($username!=ADMIN_USERNAME || $password!=ADMIN_PSW){
-    echo "<script type='text/javascript'>alert('Login failed'); window.location = '/NLCS'</script>";
+    echo "<script type='text/javascript'>alert('Login failed'); window.location = '/'</script>";
 }
 else{
     setcookie('user','admin',time()+60*60*24*30);
-    echo "<script>window.location.replace('/NLCS/admin.php');</script>";
+    echo "<script>window.location.replace('admin.php');</script>";
 }
